7e81be315883c0b69eb61486a71c5007cbf03e8f
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-11-25 22:43:01 +0100

    Added event listener

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ const button = document.querySelector('button');
 const color = ['red', 'blue', 'green', 'gray', 'yellow', 'violet'];
 
 body.style.backgroundColor = 'orange';
-button.addEventListener = ('click', changeColor);
+button.addEventListener ('click', changeColor);
 
 function changeColor (){
 	const colorIndex = parseInt(Math.random() * color.length);
 	body.style.backgroundColor = color[colorIndex];
 }
+
 
 
 
