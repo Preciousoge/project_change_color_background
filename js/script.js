a9c46184d30619fa9689f3d6c785ba11d6fdc715
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-11-25 22:40:42 +0100

    updated the methods

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const body = document.querySelector('body');
 const button = document.querySelector('button');
 const color = ['red', 'blue', 'green', 'gray', 'yellow', 'violet'];
 
-body.style.backgroundColor = 'orange'
+body.style.backgroundColor = 'orange';
 button.addEventListener = ('click', changeColor);
 
 function changeColor (){
-	const colorIndex = parseInt(Math.random * color.length);
-	body.style.backgroundColor = color[colorIndex]
+	const colorIndex = parseInt(Math.random() * color.length);
+	body.style.backgroundColor = color[colorIndex];
 }
 
 
