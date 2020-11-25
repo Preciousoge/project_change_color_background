9b3c57e1934b1a2cc15ca190f4828dc55ac00ef9
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-11-25 22:48:04 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const body = document.querySelector('body');
+/*const body = document.querySelector('body');
 const button = document.querySelector('button');
 const color = ['red', 'blue', 'green', 'gray', 'yellow', 'violet'];
 
@@ -9,10 +9,22 @@ button.addEventListener ('click', changeColor);
 function changeColor (){
 	const colorIndex = parseInt(Math.random() * color.length);
 	body.style.backgroundColor = color[colorIndex];
+}*/
+
+
+
+
+//Choose a random color
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+body.style.backgroundColor = 'violet'
+button.addEventListener('click', changeBackground)
+
+function changeBackground(){
+const colorIndex= parseInt(Math.random()*colors.length)
+body.style.backgroundColor = colors[colorIndex]
 }
-
-
-
-
 
 
