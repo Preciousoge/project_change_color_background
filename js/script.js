ed18075b2c930c75c5309b7c03b86033d2745a9e
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-11-25 22:37:17 +0100

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-//Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
+const body = document.querySelector('body');
+const button = document.querySelector('button');
+const color = ['red', 'blue', 'green', 'gray', 'yellow', 'violet'];
 
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+body.style.backgroundColor = 'orange'
+button.addEventListener = ('click', changeColor);
+
+function changeColor (){
+	const colorIndex = parseInt(Math.random * color.length);
+	body.style.backgroundColor = color[colorIndex]
 }
+
+
+
